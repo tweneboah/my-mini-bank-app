75c02b7d8c5c878bd1f6c17c88f9150a613691c4
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-05-21 18:42:49 +0800

    dfdfdfdfdfdf

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const generatePassword = require('password-generator');
-const authRoutes = require('./routes/authRoutes')
 
 const app = express();
 
@@ -9,10 +8,23 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Put all API endpoints under '/api'
+app.get('/api/passwords', (req, res) => {
+  const count = 5;
 
-//Auth routes
-authRoutes(app)
+  // Generate some passwords
+  const passwords = Array.from(Array(count).keys()).map(i =>
+    generatePassword(12, false)
+  )
 
+  // Return them as json
+  res.json(passwords);
+
+  console.log(`Sent ${count} passwords`);
+});
+
+app.get('/n', (req, res) => {
+  res.send({name:' Akos'})
+})
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
@@ -24,59 +36,6 @@ const port = process.env.PORT || 5000;
 app.listen(port);
 
 console.log(`Password generator listening on ${port}`);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const express = require('express');
-// const path = require('path');
-// const generatePassword = require('password-generator');
-
-// const app = express();
-
-// // Serve static files from the React app
-// app.use(express.static(path.join(__dirname, 'client/build')));
-
-// // Put all API endpoints under '/api'
-// app.get('/api/passwords', (req, res) => {
-//   const count = 5;
-
-//   // Generate some passwords
-//   const passwords = Array.from(Array(count).keys()).map(i =>
-//     generatePassword(12, false)
-//   )
-
-//   // Return them as json
-//   res.json(passwords);
-
-//   console.log(`Sent ${count} passwords`);
-// });
-
-// app.get('/n', (req, res) => {
-//   res.send({name:' Akos'})
-// })
-
-// // The "catchall" handler: for any request that doesn't
-// // match one above, send back React's index.html file.
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname+'/client/build/index.html'));
-// });
-
-// const port = process.env.PORT || 5000;
-// app.listen(port);
-
-// console.log(`Password generator listening on ${port}`);
 
 
 
