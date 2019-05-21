aafff2bd8cc654695cb5714192a0d89a67614bcf
Emmanuel Tweneboah <twentekghana@gmail.com>
2019-05-21 20:05:07 +0800

    jdshdjsdhs dsfdf

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
   res.send({name: 'Emmanuel Famil'})
 })
 // Serve static files from the React app
-// app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // // Put all API endpoints under '/api'
 // app.get('/api/passwords', (req, res) => {
@@ -28,9 +28,9 @@ app.get('/', (req, res) => {
 
 // // The "catchall" handler: for any request that doesn't
 // // match one above, send back React's index.html file.
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname+'/client/build/index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port);
